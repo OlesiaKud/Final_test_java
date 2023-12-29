3c738f087ef4a63c81d8f210957d883af50f2e20
Olesya Kudr <ala.kud00@mail.ru>
2023-12-29 12:42:43 +0700

    добавила комментарии, позже поработать с очередями

--- untitled/src/view/View.java
+++ untitled/src/view/View.java
@@ -13,6 +13,7 @@ import java.util.*;
 
 public class View {
     public void main_menu() {
+//        создаем экземпляры розыгрыша игрушек
         Lottery car_Lottery = new Car_lottery();
         Lottery doll_Lottery = new Doll_lottery();
         Lottery lego_Lottery = new Lego_lottery();
@@ -20,29 +21,28 @@ public class View {
 
         Queue<Toy> queue = new PriorityQueue<>((t1, t2) -> t2.getRate() - t1.getRate());
         System.out.println("Розыгрыш призов в магазине игрушек");
-        queue.offer(car_Lottery.lottery(1, "машинка" , 5, 20));
-        queue.offer(doll_Lottery.lottery(1, "куколка" , 5, 30));
-        queue.offer(lego_Lottery.lottery(1, "lego", 3, 50));
-        queue.offer(teddy_Lottery.lottery(1, "медвежонок" , 2, 80));
+//        добавляем в очередь работает и без очереди
+//        queue.offer(car_Lottery.lottery(1, "машинка" , 5, 20));
+//        queue.offer(doll_Lottery.lottery(2, "куколка" , 5, 30));
+//        queue.offer(lego_Lottery.lottery(3, "lego", 3, 50));
+//        queue.offer(teddy_Lottery.lottery(4, "медвежонок" , 2, 80));
         Random random = new Random();
+//        добавляем в файл
         try(FileWriter writer = new FileWriter("D:\\study\\final_test\\task_2\\untitled\\src\\toy_lottery.txt");) {
             Toy toy = null;
             for (int i = 0; i < 10; i++) {
                 int randomValue = random.nextInt(100);
-
                 if (randomValue <= 20) {
-                    toy = teddy_Lottery.lottery(1, "медвежонок" , 10, 80);
+                    toy = teddy_Lottery.lottery(4, "медвежонок" , 10, 80);
                 } else if  (randomValue <= 50) {
-                    toy = lego_Lottery.lottery(1, "lego", 30, 50);
+                    toy = lego_Lottery.lottery(3, "lego city", 30, 50);
                 } else if (randomValue <= 70) {
-                    toy = doll_Lottery.lottery(1, "куколка" , 50, 20);
+                    toy = doll_Lottery.lottery(2, "куколка-фея" , 50, 20);
                 } else if (randomValue <= 80) {
-                    toy = car_Lottery.lottery(1, "машинка", 50, 10);
+                    toy = car_Lottery.lottery(1, "машинка lada", 50, 10);
                 }
-                String prize = "ID: " + toy.getId() +", название: " + toy.getName() + ", вес в % от 100: "+ toy.getRate() + "\n";
-                writer.write(prize);
+                writer.write(toy.toString());
             }
-
             System.out.println("Розыгрыш прошёл успешно, данные записаны в файл toy_lottery.txt");
         } catch (IOException e) {
             System.out.println("Ошибка при записи файла");
