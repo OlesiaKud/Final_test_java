59b45637e84e99bd9c6e46bf67b73d7143d04002
Olesya Kudr <ala.kud00@mail.ru>
2023-12-30 11:45:42 +0700

    сделала вывод в файл при помощи PriorityQueue<>

--- untitled/src/view/View.java
+++ untitled/src/view/View.java
@@ -18,39 +18,61 @@ public class View {
         Lottery doll_Lottery = new Doll_lottery();
         Lottery lego_Lottery = new Lego_lottery();
         Lottery teddy_Lottery = new Lego_lottery();
-
-        Queue<Toy> queue = new PriorityQueue<>((t1, t2) -> t2.getRate() - t1.getRate());
         System.out.println("Розыгрыш призов в магазине игрушек");
-//        добавляем в очередь работает и без очереди
-//        queue.offer(car_Lottery.lottery(1, "машинка" , 5, 20));
-//        queue.offer(doll_Lottery.lottery(2, "куколка" , 5, 30));
-//        queue.offer(lego_Lottery.lottery(3, "lego", 3, 50));
-//        queue.offer(teddy_Lottery.lottery(4, "медвежонок" , 2, 80));
-        Random random = new Random();
-//        добавляем в файл
+        Queue<Toy> queue = new PriorityQueue<>((t2, t1) -> t2.getRate() - t1.getRate());
+
+//запрашиваем сколько будет разных видов игрушек участвовать в розыгрыше и добавляем их в очередь
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество машинок(число) :");
+        try {
+            int count = scanner.nextInt();
+            for (int i = 0; i < count; i++) {
+                queue.offer(car_Lottery.lottery(1, "машинка" , count, 20));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка преобразования строки в число");
+        }
+
+        System.out.println("Введите количество кукол(число) :");
+        try {
+            int count = scanner.nextInt();
+            for (int i = 0; i < count; i++) {
+                queue.offer(doll_Lottery.lottery(2, "куколка-фея" , count, 30));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка преобразования строки в число");
+        }
+
+        System.out.println("Введите количество конструкторов(число) :");
+        try {
+            int count = scanner.nextInt();
+            for (int i = 0; i < count; i++) {
+                queue.offer(lego_Lottery.lottery(3, "lego", count, 50));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка преобразования строки в число");
+        }
+        System.out.println("Введите количество мягких игрушек(число) :");
+        try {
+            int count = scanner.nextInt();
+            for (int i = 0; i < count; i++) {
+                queue.offer(teddy_Lottery.lottery(4, "медвежонок" , count, 80));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка преобразования строки в число");
+        }
+        scanner.close();
+//        добавляем в файл по порядку выхода из очереди - выигрывают первые, те что легче.
+//        Изменить можно в строке 22 "Queue<Toy> queue = new PriorityQueue<>((t2, t1) -> t2.getRate() - t1.getRate());"
         try(FileWriter writer = new FileWriter("D:\\study\\final_test\\task_2\\untitled\\src\\toy_lottery.txt");) {
-            Toy toy = null;
             for (int i = 0; i < 10; i++) {
-                int randomValue = random.nextInt(100);
-                if (randomValue <= 20) {
-                    toy = teddy_Lottery.lottery(4, "медвежонок" , 10, 80);
-                } else if  (randomValue <= 50) {
-                    toy = lego_Lottery.lottery(3, "lego city", 30, 50);
-                } else if (randomValue <= 70) {
-                    toy = doll_Lottery.lottery(2, "куколка-фея" , 50, 20);
-                } else if (randomValue <= 80) {
-                    toy = car_Lottery.lottery(1, "машинка lada", 50, 10);
-                }
-                writer.write(toy.toString());
+                writer.write(String.valueOf(queue.poll()));
             }
             System.out.println("Розыгрыш прошёл успешно, данные записаны в файл toy_lottery.txt");
         } catch (IOException e) {
             System.out.println("Ошибка при записи файла");
             e.printStackTrace();
         }
-
     }
-
-
-
 }
